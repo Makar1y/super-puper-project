dbf13f18cca4acaab0c7645360d0baf5e7d542aa
Makar1y <112717875+Makar1y@users.noreply.github.com>
2025-12-01 22:23:24 +0200

    add tag searching

--- modules/tmpEngine.c
+++ modules/tmpEngine.c
@@ -2,34 +2,52 @@
 #include <stdlib.h>
 #include <string.h>
 
-// [ ] Add documentation
+// TODO Add documentation
 #define LINE_BUFFER_SIZE 256 // Max buffer length (while scaning template file)
 #define MAX_REQUEST_URL_LENGTH 256
 
 #define ERROR_PREFIX  "!!! ERROR: "
+#define BLOCK_PREFIX "{%"
+#define BLOCK_POSTFIX "%}"
+#define TAG_PREFIX "{{"
+#define TAG_POSTFIX "}}"
 
 
 int template_to_html(FILE *html_template, char *target_filename) {
+    unsigned int line = 0;
     char *buffer = malloc(sizeof(char) * LINE_BUFFER_SIZE);
     char *request_url = calloc(MAX_REQUEST_URL_LENGTH, sizeof(char));
+    *request_url = '.';
 
     while (fgets(buffer, LINE_BUFFER_SIZE, html_template)) {
-        char *block_name_start = strstr(buffer, "{%");
+        ++line;
+        char *block_name_start = strstr(buffer, "{%"); // If start of block in line
+        char *tag_name_start = strstr(buffer, "{{"); // if start of tag
 
-        if (block_name_start) {
+        if (block_name_start) { // block serving
             char *block_name_end = strstr(buffer, "%}");
 
             if (block_name_end) {
                 char block_name[LINE_BUFFER_SIZE];
                 if (sscanf(block_name_start, "{%% %[^%{}] %%}", block_name)) {
-                    printf("Ok, %s\n", block_name);
-                } else {
-                    fprintf(stderr, "%sNo block name provided.\n", ERROR_PREFIX);
-                }
-            } // TODO Error
-        } // TODO Error
+                    printf("%s%u, ", block_name, line);
+
+                } else { fprintf(stderr, "%sNo block name provided after opened '%s' on line %u !\n", ERROR_PREFIX, BLOCK_PREFIX, line); } // Error
+            } else { fprintf(stderr, "%sBlock not closed with '%s' !\n", ERROR_PREFIX, BLOCK_POSTFIX, line); } // Error
+        }
+
+        if (tag_name_start) { // tag serving
+            char *tag_name_end = strstr(buffer, "}}");
+
+            if (tag_name_end) {
+                char tag_name[LINE_BUFFER_SIZE];
+                if (sscanf(tag_name_start, "{{ %[^{}] }}", tag_name)) {
+                    printf("%s %u, ", tag_name, line);
+                    // FIXME Add checks for array elements
+                } else { fprintf(stderr, "%sNo tag name provided after opened '%s' on line %u!\n", ERROR_PREFIX, TAG_PREFIX, line); } // Error 
+            } else { fprintf(stderr, "%sTag not closed with '%s' on line %u !\n", ERROR_PREFIX, TAG_POSTFIX, line); } // Error
+        }
     }
-    printf("%s", buffer);
 }
 
 int main() {
