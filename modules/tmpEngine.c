9225a847d278f4317669fef8dea8a4877890d44b
Makar1y <112717875+Makar1y@users.noreply.github.com>
2025-12-01 22:36:25 +0200

    some improvements

--- modules/tmpEngine.c
+++ modules/tmpEngine.c
@@ -9,8 +9,10 @@
 #define ERROR_PREFIX  "!!! ERROR: "
 #define BLOCK_PREFIX "{%"
 #define BLOCK_POSTFIX "%}"
+#define BLOCK_NAME_SCAN_FORMAT "{%% %[^%{}] %%}"
 #define TAG_PREFIX "{{"
 #define TAG_POSTFIX "}}"
+#define TAG_NAME_SCAN_FORMAT "{{ %[^{}] }}"
 
 
 int template_to_html(FILE *html_template, char *target_filename) {
@@ -21,15 +23,16 @@ int template_to_html(FILE *html_template, char *target_filename) {
 
     while (fgets(buffer, LINE_BUFFER_SIZE, html_template)) {
         ++line;
-        char *block_name_start = strstr(buffer, "{%"); // If start of block in line
-        char *tag_name_start = strstr(buffer, "{{"); // if start of tag
+        char *block_name_start = strstr(buffer, BLOCK_PREFIX); // If start of block in line
+        char *tag_name_start = strstr(buffer, TAG_PREFIX); // if start of tag
+        fseek(html_template, -20, SEEK_CUR);
 
         if (block_name_start) { // block serving
-            char *block_name_end = strstr(buffer, "%}");
+            char *block_name_end = strstr(buffer, BLOCK_POSTFIX);
 
             if (block_name_end) {
                 char block_name[LINE_BUFFER_SIZE];
-                if (sscanf(block_name_start, "{%% %[^%{}] %%}", block_name)) {
+                if (sscanf(block_name_start, BLOCK_NAME_SCAN_FORMAT, block_name)) {
                     printf("%s%u, ", block_name, line);
 
                 } else { fprintf(stderr, "%sNo block name provided after opened '%s' on line %u !\n", ERROR_PREFIX, BLOCK_PREFIX, line); } // Error
@@ -37,11 +40,11 @@ int template_to_html(FILE *html_template, char *target_filename) {
         }
 
         if (tag_name_start) { // tag serving
-            char *tag_name_end = strstr(buffer, "}}");
+            char *tag_name_end = strstr(buffer, TAG_POSTFIX);
 
             if (tag_name_end) {
                 char tag_name[LINE_BUFFER_SIZE];
-                if (sscanf(tag_name_start, "{{ %[^{}] }}", tag_name)) {
+                if (sscanf(tag_name_start, TAG_NAME_SCAN_FORMAT, tag_name)) {
                     printf("%s %u, ", tag_name, line);
                     // FIXME Add checks for array elements
                 } else { fprintf(stderr, "%sNo tag name provided after opened '%s' on line %u!\n", ERROR_PREFIX, TAG_PREFIX, line); } // Error 
